a0ed5ef5cb7caf4897b5dd3a96e9e2efbabb385f
tanCelena <tan.celena@gmail.com>
2012-11-28 17:06:08 -0800

    =)

--- wProxy.h
+++ wProxy.h
@@ -1,6 +1,6 @@
 /* Name & E-mail: Celena Tan            tan.celena@gmail.com
  * Name & E-mail: Aseda Aboagye         aseda.aboagye@gmail.com
- * File Name: wProxy.h
+ * File Name: main.c
  * Course: COMP 177 Computer Networking
  * Project: Web Proxy
  * Created on: November 15, 2012
@@ -10,6 +10,15 @@
 #ifndef WPROXY_H_
 #define WPROXY_H_
 #include <argp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 struct arguments {
     int verbose;
@@ -17,6 +26,17 @@ struct arguments {
     char *security;
 };
 
+struct host_info {
+    char *host;
+    char *ip;
+    int port;
+    char *path;
+    char *user;
+};
+
 static error_t parse_opt (int key, char *arg, struct argp_state *state);
 
+int security (struct host_info *h);
+
 #endif
+
